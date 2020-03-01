1d005752e3d006a6025417f7d004ecadc680fc9b
pentiumv2020 <61608027+pentiumv2020@users.noreply.github.com>
2020-03-01 19:31:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+./scripts/feeds update -a
+./scripts/feeds install -a
